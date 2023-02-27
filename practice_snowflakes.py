bb031f9828f71b755a59664da75ed2059a0c5a6b
Srilakshmi-Movva <126463539+Srilakshmi-Movva@users.noreply.github.com>
2023-02-27 19:38:11 +0530

    Update practice_snowflakes.py
    added one more header for a special menu

--- practice_snowflakes.py
+++ practice_snowflakes.py
@@ -6,4 +6,4 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avacado toast')
 
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
