1535495c6d322ffb4070f584eb53588495e2e91b
Srilakshmi-Movva <126463539+Srilakshmi-Movva@users.noreply.github.com>
2023-02-27 19:34:49 +0530

    Update practice_snowflakes.py
    added breakfast header and add 3 items

--- practice_snowflakes.py
+++ practice_snowflakes.py
@@ -1,6 +1,6 @@
 import streamlit;
 streamlit.title('My Parents new Healthy diner')
-stremalit.header('Breakfast Menu')
-stremalit.text('Omega 3 & Blueberry oatmeal')
-stremalit.text('kale, spinach & rocket smoothie')
-stremalit.text('Hard-boiled free range egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
