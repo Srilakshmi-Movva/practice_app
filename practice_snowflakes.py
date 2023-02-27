7eac077c549b645619d971441f73a9d71be352ed
Srilakshmi-Movva <126463539+Srilakshmi-Movva@users.noreply.github.com>
2023-02-27 19:33:13 +0530

    Update practice_snowflakes.py
    fixed typo

--- practice_snowflakes.py
+++ practice_snowflakes.py
@@ -1,2 +1,6 @@
 import streamlit;
 streamlit.title('My Parents new Healthy diner')
+stremalit.header('Breakfast Menu')
+stremalit.text('Omega 3 & Blueberry oatmeal')
+stremalit.text('kale, spinach & rocket smoothie')
+stremalit.text('Hard-boiled free range egg')
